af5d7794c029ba8034b8504b637bbb7afd84b0f2
Kavindu Vidushan Karunathilaka <143307455+karuna3laka@users.noreply.github.com>
2024-12-24 20:45:39 +0530

    k

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Float
+import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))  # take absolute location of the file
 
 
 @app.route('/')  # decorator.special capability...define route for endpoint
@@ -10,21 +14,36 @@ def hello_world():  # put application's code here
 
 @app.route('/super_simple')
 def super_simple():
-    return jsonify(meassage='yahoo122') #make these valid jason
+    return jsonify(meassage='yahoo122')  # make these valid jason
+
 
 @app.route('/not_found')
 def not_found():
     return jsonify(message='That resource are not found !'), 404
 
+
 @app.route('/parameters')
 def parameters():
     name = request.args.get('name')
     age = int(request.args.get('age'))
-    if age < 18 :
+    if age < 18:
         return jsonify(message="Sorry " + name + ", you are not Old enough"), 401
     else:
         return jsonify(message="Welcome " + name + ", you are old Enough")
 
 
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/url_variable/<string:name>/<int:age>')  # Corrected route definition
+def url_variables(name: str, age: int):
+    if age < 18:
+        return jsonify(message="Sorry " + name + ", you are not old enough"), 401
+    else:
+        return jsonify(message="Welcome " + name + ", you are old enough"), 201
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
